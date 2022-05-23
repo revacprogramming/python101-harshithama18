ffb39d4609706c9b16db1f4aec80187a96095a53
harshithama18 <97089315+harshithama18@users.noreply.github.com>
2022-05-23 18:56:57 +0000

    P7

--- ActivitySet#1/problem#07.py
+++ ActivitySet#1/problem#07.py
@@ -1,3 +1,8 @@
 # Strings
 
-text = "X-DSPAM-Confidence:    0.8475"
+text = 'X-DSPAM-Confidence:    0.8475'
+colonpos = text.find(':')
+sppos = text.find('    ',colonpos)
+host = text[colonpos+4:]
+number = float(host)
+print(number)
