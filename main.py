5bd201b680ce9915bcd973077808b3c8407db7f0
harshithama18 <97089315+harshithama18@users.noreply.github.com>
2022-05-29 18:25:23 +0000

    changes

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
 # Use the file name mbox-short.txt as the file name
-fname = input("Enter file name: ")
-fh = open(fname)
-count = 0
-for line in fh:
-    if line.startswith("X-DSPAM-Confidence:"):
-       count = count + 1 
-print('Line count',count) 
-colonpos = fh.find('X-DSPAM-Confidence:')
-host = fh[colonpos+1:]
-avg = host/count
-print('Average spam confidence: ')
-
+large = a
+if a>c:=b 
+    else c
+if a>
