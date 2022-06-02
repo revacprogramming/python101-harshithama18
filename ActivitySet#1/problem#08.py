abd0c1b7c4b73b909a87a71bd0b82785c8bf4392
harshithama18 <97089315+harshithama18@users.noreply.github.com>
2022-06-02 17:56:47 +0000

    changessss

--- ActivitySet#1/problem#08.py
+++ ActivitySet#1/problem#08.py
@@ -1,21 +1,22 @@
-# Files
-
+# Files 
+\
 #filename = "dataset/mbox-short.txt"
-
   # Use the file name mbox-short.txt as the file name
+# Use the file name mbox-short.txt as the file name
 fname = input("Enter file name: ")
 fh = open(fname)
 inp = fh.read()
 if inp.startswith('X-DSPAM-Confidence:    '):
-    count = 0
+    while True:
+        count = 0
     for line in inp:
         count = count + 1  
 if inp.startswith('X-DSPAM-Confidence:    '):
+    while True:
     sum = 0
     colonpos = inp.find(':')
     host = inp[colonpos+4:]
-    for number in host:
-        addi = float(add) + 1
-card = float(addi)
-avg = card/count
-print('Average spam confidence: %d',avg)                                                                         
+    temp = float(host)
+    sum = sum + temp
+avg = sum/count
+print('Average spam confidence: %d',avg)                                                                        
