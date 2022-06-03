cc0e26296efcfc89d4ee304fbdb9c481d20549d9
harshithama18 <97089315+harshithama18@users.noreply.github.com>
2022-06-03 18:25:35 +0000

    changeddd

--- ActivitySet#1/problem#08.py
+++ ActivitySet#1/problem#08.py
@@ -2,21 +2,21 @@
 \
 #filename = "dataset/mbox-short.txt"
   # Use the file name mbox-short.txt as the file name
-# Use the file name mbox-short.txt as the file name
+
 fname = input("Enter file name: ")
 fh = open(fname)
-inp = fh.read()
+for line in fh:
+    line = line.rstrip()
+    if not line.startswith('X-DSPAM-Confidence:    '): 
+        continue
+        numbers = line.split()
+        store = line[:]
 if inp.startswith('X-DSPAM-Confidence:    '):
     while True:
-        count = 0
-    for line in inp:
-        count = count + 1  
-if inp.startswith('X-DSPAM-Confidence:    '):
-    while True:
-    sum = 0
-    colonpos = inp.find(':')
-    host = inp[colonpos+4:]
-    temp = float(host)
-    sum = sum + temp
-avg = sum/count
-print('Average spam confidence: %d',avg)                                                                        
+     total = 0
+     colonpos = inp.find(':')
+     host = inp[colonpos+4:]
+     temp = float(host)
+     total = total + temp
+avg = total/count
+print('Average spam confidence: %d',avg)                                                                                                                                              
--- ActivitySet#1/problem#09.py
+++ ActivitySet#1/problem#09.py
@@ -1,3 +1,3 @@
 # Lists
 
-filename = "dataset/romeo.txt"
+#filename = "dataset/romeo.txt"
