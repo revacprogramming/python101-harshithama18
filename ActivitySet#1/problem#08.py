66e861be5d5dd90d74800c75527fcdf0850d1d48
harshithama18 <97089315+harshithama18@users.noreply.github.com>
2022-06-22 18:22:23 +0000

    Change 4

--- ActivitySet#1/problem#08.py
+++ ActivitySet#1/problem#08.py
@@ -8,11 +8,12 @@ fh = open(fname)
 count = 0
 total = 0
 for line in fh:
-    if not line.startswith('X-DSPAM-Confidence: '): 
+    if not line.startswith('X-DSPAM-Confidence:'): 
         continue
-t = find.line(" ")
-  
-  count+ = 1
-  total+ = number  
+    t = line.find("0")
+    number = float(line[t:])
+    count += 1
+    total = total + number
+        #count =float(count)
 avg = total/count
-
+print(f'Average spam confidence: {avg}') 
