3bdafdf2ad81bac41610cced650b598eaaa2da4a
harshithama18 <97089315+harshithama18@users.noreply.github.com>
2022-07-04 13:26:50 +0000

    Change 10

--- ActivitySet#1/problem#10.py
+++ ActivitySet#1/problem#10.py
@@ -13,3 +13,27 @@ for length, word in t:
     res.append(word)
 print(res)
 """
+"""
+name = input("Enter file:")
+if len(name) < 1:
+    name = "mbox-short.txt"
+handle = open(name)
+"""
+fname = input('Enter the file name:')
+fh = open(fname)
+counts = dict()
+for line in fh:
+    if line startswith ("From "):
+        words = line.split()
+        for word in words:
+            if word[2] not in counts:
+                counts[word] = 1
+            else:
+                counts[word] += 1
+lst = list()
+for key,val in list(counts.tems()):
+    lst.append((val,key))
+lst.sort(reverse=True)
+for key,val in lst
+
+print()
