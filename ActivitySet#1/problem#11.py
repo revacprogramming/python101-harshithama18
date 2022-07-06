d549fb1a0b555c16af762000919f9263a4c20396
harshithama18 <97089315+harshithama18@users.noreply.github.com>
2022-07-06 19:37:51 +0000

    p11

--- ActivitySet#1/problem#11.py
+++ ActivitySet#1/problem#11.py
@@ -6,3 +6,18 @@ def print_twice(bruce):
     print(bruce)
     print(bruce)
 """
+fname = input("Enter the name of the file: ")
+fh = open(fname)
+d = dict()
+for line in fh:
+    if line.startswith("From "):
+        line = line.split()
+        line = line[5]
+        line = line[0:2]
+        d[line] = d.get(line,0)+1
+lst = list()
+for value,count in d.items():
+    lst.append((value,count))
+lst.sort()
+for value,count in lst:
+    print(value,count)
