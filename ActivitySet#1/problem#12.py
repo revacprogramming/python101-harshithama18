7d65e89ddf806e53dc360df80b14567bf4644b1f
harshithama18 <97089315+harshithama18@users.noreply.github.com>
2022-07-08 16:52:29 +0000

    Change 13

--- ActivitySet#1/problem#12.py
+++ ActivitySet#1/problem#12.py
@@ -1,6 +1,6 @@
 # Regular Expressions
 # https://www.py4e.com/lessons/regex
-"""
+
 import re
 fname = input("Enter ")
 hand = open(fname)
@@ -8,17 +8,3 @@ for line in hand:
     line = line.rstrip()
     if re.search('^F..m:.+za', line):
         print(line)
-"""
-import re
-
-hand = open("regex_sum_24962.txt")
-x=list()
-for line in hand:
-     y = re.findall('[0-9]+',line)
-     x = x+y
-
-sum=0
-for z in x:
-    sum = sum + int(z)
-
-print(sum)
--- ActivitySet#1/problem#14.py
+++ ActivitySet#1/problem#14.py
@@ -1,24 +1,2 @@
 # Using Web Services
 # https://www.py4e.com/lessons/servces
-import urllib
-import xml.etree.ElementTree as ET
-
-url = raw_input("Enter location: ")
-if len(url) < 1:
-    url = "http://python-data.dr-chuck.net/comments_200531.xml"
-print "Retrieving " + url
-
-xml = urllib.urlopen(url).read()
-print "Retrieved: " + str(len(xml)) + " characters"
-
-tree = ET.fromstring(xml)
-
-counts =  tree.findall('.//count')
-print "Count: " + str(len(counts))
-
-accumulator = 0
-
-for count in counts:
-    accumulator += int(count.text)
-
-print "Sum:" + str(accumulator)
