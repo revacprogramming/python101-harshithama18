0522d36c96c8a99a16b9be4d8b760d9ce173a572
harshithama18 <97089315+harshithama18@users.noreply.github.com>
2022-07-19 10:47:12 +0000

    Change 23

--- ActivitySet#1/problem#14.py
+++ ActivitySet#1/problem#14.py
@@ -1,24 +1,52 @@
 # Using Web Services
-import xml.etree.ElementTree as ET
+import urllib.request, urllib.parse, urllib.error
+import json
+import ssl
 
-input = '''
-<stuff>
-  <users>
-    <user x="2">
-      <id>001</id>
-      <name>Chuck</name>
-    </user>
-    <user x="7">
-      <id>009</id>
-      <name>Brent</name>
-    </user>
-  </users>
-</stuff>'''
+api_key = False
+# If you have a Google Places API key, enter it here
+# api_key = 'AIzaSy___IDByT70'
+# https://developers.google.com/maps/documentation/geocoding/intro
 
-stuff = ET.fromstring(input)
+if api_key is False:
+    api_key = 42
+    serviceurl = 'http://py4e-data.dr-chuck.net/json?'
+else :
+    serviceurl = 'https://maps.googleapis.com/maps/api/geocode/json?'
 
-lst = stuff.findall('users/user')
-print('User count:', len(lst))
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
 
-lst2 = stuff.findall('user')
-print('User count:', len(lst2))
+while True:
+    address = input('Enter location: ')
+    if len(address) < 1: break
+
+    parms = dict()
+    parms['address'] = address
+    if api_key is not False: parms['key'] = api_key
+    url = serviceurl + urllib.parse.urlencode(parms)
+
+    print('Retrieving', url)
+    uh = urllib.request.urlopen(url, context=ctx)
+    data = uh.read().decode()
+    print('Retrieved', len(data), 'characters')
+
+    try:
+        js = json.loads(data)
+    except:
+        js = None
+
+    if not js or 'status' not in js or js['status'] != 'OK':
+        print('==== Failure To Retrieve ====')
+        print(data)
+        continue
+
+    print(json.dumps(js, indent=4))
+
+    lat = js['results'][0]['geometry']['location']['lat']
+    lng = js['results'][0]['geometry']['location']['lng']
+    print('lat', lat, 'lng', lng)
+    location = js['results'][0]['formatted_address']
+    print(location)
